d4a4d344034950a9e4ba81dca15ecaf802a9142d
Vinicius Raony <viniciusraony@hotmail.com>
2023-05-11 02:42:49 -0300

    Fix Manifest

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'pwaKazumi+-v1.2';
+var cacheName = 'ToDoList';
 
 self.addEventListener('install', event => {
 
@@ -19,40 +19,7 @@ self.addEventListener('install', event => {
                 './assets/img/icon_114.png',
                 './assets/img/icon_120.png',
                 './assets/img/icon_180.png',
-                './assets/img/icon_512.png',
-
-                './assets/img/about.webp',
-                './assets/img/about-2.webp',
-                './assets/img/destaque1.webp',
-                './assets/img/destaque2.webp',
-                './assets/img/destaque3.webp',
-                './assets/img/events-1.webp',
-                './assets/img/events-2.webp',
-                './assets/img/events-3.webp',
-                './assets/img/reservation.webp',
-                './assets/img/stats-bg.webp',
-
-                './assets/img/gallery/gallery-1.webp',
-                './assets/img/gallery/gallery-2.webp',
-                './assets/img/gallery/gallery-3.webp',
-                './assets/img/gallery/gallery-4.webp',
-                './assets/img/gallery/gallery-5.webp',
-                './assets/img/gallery/gallery-6.webp',
-                './assets/img/gallery/gallery-7.webp',
-                './assets/img/gallery/gallery-8.webp',
-
-                './assets/img/menu/menu-item-1.webp',
-                './assets/img/menu/menu-item-2.webp',
-                './assets/img/menu/menu-item-3.webp',
-                './assets/img/menu/menu-item-4.webp',
-                './assets/img/menu/menu-item-5.webp',
-                './assets/img/menu/menu-item-6.webp',
-
-                './assets/img/testimonials/testimonials-1.webp',
-                './assets/img/testimonials/testimonials-2.webp',
-                './assets/img/testimonials/testimonials-3.webp',
-                './assets/img/testimonials/testimonials-4.webp',
-
+                './assets/img/icon_512.png',             
             ]))
     );
 });
